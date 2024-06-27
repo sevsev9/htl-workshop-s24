3fb6dfa7c858192cd8987234ca5020699beed711
sevsev9 <sevm@pm.me>
2024-06-27 12:29:38 +0200

    feat: added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+
 import envUtil from './util/env.util';
 import router from './routes';
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/", router)
 
