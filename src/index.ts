3864de57f18a49a31ece8ceb71ba1f1624339064
sevsev9 <sevm@pm.me>
2024-06-27 12:05:05 +0200

    feat: add router config with example user router

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import express from 'express';
 import envUtil from './util/env.util';
+import router from './routes';
 
-console.log(envUtil.PORT);
-console.log(typeof envUtil.PORT);
+const app = express();
+
+app.use(express.json());
+
+app.use("/", router)
+
+app.listen(envUtil.PORT, () => {
+    console.log('Server started.');
+});
