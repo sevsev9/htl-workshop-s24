567f9c655a6cb6ced39f2ed9473e5f2c8d050b40
sevsev9 <sevm@pm.me>
2025-01-30 11:37:51 +0100

    feat: added session model

--- backend/src/util/jwt.util.ts
+++ backend/src/util/jwt.util.ts
@@ -7,3 +7,45 @@ export function signJwt(object: UserJwtPayload, options?: jwt.SignOptions): stri
         ...(options || {})
     });
 }
+
+
+/**
+ * Verifies a JWT using the public key.
+ * @param {string} token - The JWT to verify.
+ * @returns {object} An object containing the verification result.
+ * @property {boolean} valid - Indicates if the JWT is valid.
+ * @property {boolean} expired - Indicates if the JWT is expired.
+ * @property {object|null} decoded - The decoded JWT payload, or null if the JWT is not valid.
+ * @property {string|null} error - The error message, or null if the JWT is valid.
+ */
+export function verifyJwt(token: string): VerifyJwtResult {
+    try {
+        const decoded = jwt.verify(token, process.env.PUBLIC_KEY!) as UserJwtPayload;
+
+        return {
+            valid: true,
+            expired: false,
+            decoded,
+            error: null
+        }
+    } catch (e: any) {
+        let error = 'Invalid token';
+        let decoded;
+
+        if (e instanceof jwt.TokenExpiredError) {
+            error = 'Token expired';
+            decoded = jwt.decode(token) as UserJwtPayload;
+        } else if (e instanceof jwt.NotBeforeError) {
+            error = 'Token not active';
+        } else if (e instanceof jwt.JsonWebTokenError) {
+            error = e.message;
+        }
+
+        return {
+            valid: false,
+            expired: e instanceof jwt.TokenExpiredError,
+            decoded,
+            error
+        };
+    }
+}
