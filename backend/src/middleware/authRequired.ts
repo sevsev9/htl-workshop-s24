cac560f1091ba501bdddceee4febd8ee86a3a8e7
sevsev9 <sevm@pm.me>
2025-01-30 16:16:27 +0100

    fix: restructure auth required middleware

--- backend/src/middleware/authRequired.ts
+++ backend/src/middleware/authRequired.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import logger from "../util/logger.util";
 import { verifyJwt } from "../util/jwt.util";
+import { ApplicationError } from "../types/errors";
 
 export default function authRequiredMiddleware(
   req: Request,
@@ -14,28 +15,36 @@ export default function authRequiredMiddleware(
   if (!token) {
     logger.debug("No token provided");
 
-    return next();
+    return res.status(403).json({ message: "No JWT provided!" });
   }
 
-  const { valid, expired, decoded, error } = verifyJwt(token);
+  try {
+    const { valid, expired, decoded, error } = verifyJwt(token);
 
-  if (decoded) {
-    res.locals.user = decoded;
+    if (decoded) {
+      res.locals.user = decoded;
 
-    if (!expired) {
-      logger.debug(
-        `{Deserialize User} - User ${decoded.id} deserialized from JWT.`
-      );
-
-      return next();
-    } else {
+      if (!expired) {
         logger.debug(
-            `{Deserialize User} - User ${decoded.id} jwt expired.`
+          `{Deserialize User} - User ${decoded.id} deserialized from JWT.`
         );
 
-        return res.status(401).json({ message: "JWT Expired.", expired: true });
-    }
-  }
+        return next();
+      } else {
+        logger.debug(`{Deserialize User} - User ${decoded.id} jwt expired.`);
 
-  return res.status(401).json({ message: "Unauthorized", error });
+        return res.status(401).json({ message: "JWT Expired.", expired: true });
+      }
+    }
+
+    return res.status(401).json({ message: "Unauthorized", error });
+  } catch (err) {
+    if (err instanceof ApplicationError) {
+        return res.status(err.getHttpCode()).json({ message: err.message });
+    }
+
+    logger.debug(`{Deserialize User} - Error deserializing user: ${(err as Error).message}`);
+
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
